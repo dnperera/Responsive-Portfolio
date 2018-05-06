a1d899988cd913b94c9410b6d10a11b6ee446857
Dayan Perera <dayannp@gmail.com>
2018-05-05 23:56:38 -0700

    change contact form layout

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@ $('#contactForm').submit(function(event) {
 	console.log($(this).serialize());
 
 	$.ajax({
-		method: 'POST',
-		url: '//formspree.io/dayannp@gmail.com',
+		type: 'POST',
+		url: $('#contactForm').attr('action'),
 		data: $(this).serialize(),
-		datatype: 'json',
-	});
-
-	event.preventDefault();
-	$(this)
-		.get(0)
-		.reset();
-	alertify.success('Message sent.');
+	})
+		.done(function(response) {
+			console.log(response);
+			$(this)[0].reset();
+			// $(this)
+			// 	.get(0)
+			// 	.reset();
+			alertify.success('Message sent.');
+		})
+		.fail(function(data) {
+			console.log(data.responseText);
+		});
 });
