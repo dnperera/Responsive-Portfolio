cc5d3248488a6ae0bf32ea88bf13279426007fce
Dayan Perera <dayannp@gmail.com>
2018-05-06 23:39:55 -0700

    add form data submited  to the email body

--- mailer.php
+++ mailer.php
@@ -4,21 +4,22 @@ require("class.phpmailer.php");
 $mail = new PHPMailer();
 
 $mail->IsSMTP();                                      // set mailer to use SMTP
-$mail->Host = "box5176.bluehost.com";// specify main and backup server
+$mail->Host = "box5176.bluehost.com";//"mail.dayanperera.com";  // specify main and backup server
 $mail->SMTPAuth = true;     // turn on SMTP authentication
 $mail->Username = "dayannp@dayanperera.com";  // SMTP username
 $mail->Password = "Dphp94903$"; // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 
-$mail->From = "reply-support@udacity.com";
-$mail->FromName = "Davi ar Udacity";
+
+$mail->From = $_POST['email'];//"reply-support@udacity.com";
+$mail->FromName = $_POST['name'];
 $mail->AddAddress("dayannp@dayanperera.com", "Dayan Perera");
 
 $mail->WordWrap = 50;                                 // set word wrap to 50 characters
 $mail->IsHTML(true);                                  // set email format to HTML
-$mail->Subject = "Build your own webpage";
-$mail->Body    = "Knowing the fundamentals of programming lays the foundation for many careers, including data analysis, front-end, back-end, and mobile web development. By the end of the program, you'll become confident in your ability to think and problem-solve like a programmer, and you’ll be ready to dive deeper into any of those fields.";
-$mail->AltBody = "Knowing the fundamentals of programming lays the foundation for many careers, including data analysis, front-end, back-end, and mobile web development. By the end of the program, you'll become confident in your ability to think and problem-solve like a programmer, and you’ll be ready to dive deeper into any of those fields.";
+$mail->Subject = $_POST['subject'];
+$mail->Body    =  $_POST['message'];
+$mail->AltBody =  $_POST['message'];
 
 if(!$mail->Send())
 {
