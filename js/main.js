78a2011f68fc6cafaffd2b65835a564ae851a7a3
Dayan Perera <dayannp@gmail.com>
2018-05-07 22:54:30 -0700

    start footer section

--- js/main.js
+++ js/main.js
@@ -1,54 +1,56 @@
-$('#contactForm').submit(function(event) {
-	console.log($(this).serialize());
+$('#btnSubmit').click(function(event) {
 	event.preventDefault();
+	var name = $('#name').val();
+	var email = $('#email').val();
+	var subject = $('#subject').val();
+	var message = $('#message').val();
+
+	var dataString =
+		'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
+
 	$.ajax({
 		type: 'POST',
-		url: $('#contactForm').attr('action'),
-		data: $(this).serialize(),
-	})
-		.done(function(response) {
-			console.log(response);
-			//$(this)[0].reset();
-			$(this)
-				.get(0)
-				.reset();
-			alertify.success('Message sent.');
-		})
-		.fail(function(data) {
-			console.log(data.responseText);
-		});
-});
-
-$(document).ready(function() {
-	$('#submit').click(function() {
-		var name = $('#name').val();
-		var email = $('#email').val();
-		var password = $('#password').val();
-		var contact = $('#contact').val();
-		// Returns successful data submission message when the entered information is stored in database.
-		var dataString =
-			'name1=' +
-			name +
-			'&email1=' +
-			email +
-			'&password1=' +
-			password +
-			'&contact1=' +
-			contact;
-		if (name == '' || email == '' || password == '' || contact == '') {
-			alert('Please Fill All Fields');
-		} else {
-			// AJAX Code To Submit Form.
-			$.ajax({
-				type: 'POST',
-				url: 'ajaxsubmit.php',
-				data: dataString,
-				cache: false,
-				success: function(result) {
-					alert(result);
-				},
-			});
-		}
-		return false;
+		url: 'mail-deliver.php',
+		data: dataString,
+		success: function(response) {
+			//function called if your request succeeds
+			if (response === 'Message has been sent') {
+				$('#name').val('');
+				$('#email').val('');
+				$('#subject').val('');
+				$('#message').val('');
+				alertify.success('Message sent!.');
+			} else {
+				alertify.error(' Message could not send!.');
+			}
+			console.log('Response from the mail server -->', response);
+		},
+		error: function(response) {
+			//function called if your request failed
+			alertify.error(' Message could not send!.');
+			console.log('Error -->', response);
+			return false;
+		},
 	});
 });
+
+// $('#contactForm').submit(function(event) {
+// 	console.log($(this).serialize());
+// 	event.preventDefault();
+// 	$.ajax({
+// 		type: 'POST',
+// 		url: $('#contactForm').attr('action'),
+// 		data: $(this).serialize(),
+// 	})
+// 		.done(function(response) {
+// 			console.log(response);
+// 			//$(this)[0].reset();
+// 			$(this)
+// 				.get(0)
+// 				.reset();
+// 			alertify.success('Message sent.');
+// 		})
+// 		.fail(function(data) {
+// 			console.log(data.responseText);
+// 		});
+// });
